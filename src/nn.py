6843f7ca39ad4fd48eb246b552445b750734fa5b
Ethan Xie <xieethan2008@gmail.com>
2024-12-31 11:32:08 -0800

    basic CNN and conv layer

--- src/nn.py
+++ src/nn.py
@@ -29,6 +29,28 @@ class Layer:
     def parameters(self):
         return [p for neuron in self.neurons for p in neuron.parameters()]
 
+class Convolutional_Layer2D_Flatten:
+
+    def __init__(self, shapein, kernelsize, stride):
+        self.kernel = Neuron(kernelsize[0] * kernelsize[1])
+        self.kernelsize = kernelsize
+        self.shapein = shapein
+        self.stride = stride
+    
+    def __call__(self, x):
+        outs = []
+
+        for i in self.shapein[0]//self.stride:
+            for j in self.shapein[1]//self.stride:
+                inputs = [x[k+i][l+j] if k+i<self.shapein[0] and k+l<self.shapein[1] else 0 
+                          for k in self.kernelsize[0] for l in self.kernelsize[1] ]
+                outs.append(self.kernel(inputs))
+
+        return outs
+    
+    def parameters(self):
+        return [p for p in self.kernel.parameters()]
+
 class MLP:
 
     def __init__(self, nin, nouts):
@@ -45,3 +67,17 @@ class MLP:
 
     def parameters(self):
         return [p for layer in self.layers for p in layer.parameters()]
+
+class CNN:
+    def __init__(self, conv, nouts):
+        self.conv_layer = Convolutional_Layer2D_Flatten(*conv)
+        self.MLP = MLP((conv[0][0]//conv[2])*(conv[0][1]//conv[2]), nouts)
+    
+    def __call__(self, x):
+        x = self.conv_layer(x)
+        x = self.MLP(x)
+        return x
+    
+    def parameters(self):
+        return self.conv_layer.parameters() + self.MLP.parameters()
+
